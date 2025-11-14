3b85dd18c918e4d7eda3f087cae489b87f63de21
Tymek <vonautymek@gmail.com>
2025-11-14 01:25:25 +0100

    tv/server: added option to refresh cache when requesting movie details

--- agin-tv/server/src/routes/api/movies/tmdb_id.rs
+++ agin-tv/server/src/routes/api/movies/tmdb_id.rs
@@ -1,15 +1,27 @@
-use axum::{Extension, Json, extract::Path};
+use axum::{
+    Extension, Json,
+    extract::{Path, Query},
+};
+use serde::Deserialize;
+use utoipa::IntoParams;
 use utoipa_axum::{router::OpenApiRouter, routes};
 
 use crate::{
     errors::AxumResult,
     importer::{FetchPolicy, Importer, MovieDetails, tmdb::TmdbImporter},
     state::AppState,
-    util::tmdb::TmdbId,
+    util::{opt_bool_from_string, tmdb::TmdbId},
 };
 
 pub fn routes() -> OpenApiRouter<AppState> {
     OpenApiRouter::new().routes(routes!(search))
+}
+
+#[derive(Deserialize, IntoParams)]
+#[into_params(parameter_in = Query)]
+pub struct Params {
+    #[serde(default, deserialize_with = "crate::util::opt_bool_from_string")]
+    pub refresh_cache: Option<bool>,
 }
 
 /// Movie Details
@@ -19,7 +31,8 @@ pub fn routes() -> OpenApiRouter<AppState> {
     method(get),
     path = "/",
     params(
-        ("tmdb_id" = TmdbId, Path)
+        ("tmdb_id" = TmdbId, Path),
+        Params
     ),
     responses(
         (status = OK, description = "Success", body = MovieDetails)
@@ -29,10 +42,18 @@ pub fn routes() -> OpenApiRouter<AppState> {
 async fn search(
     Extension(state): Extension<AppState>,
     Path(tmdb_id): Path<TmdbId>,
+    Query(params): Query<Params>,
 ) -> AxumResult<Json<MovieDetails>> {
+    let refresh_cache = params.refresh_cache.unwrap_or(false);
+    let fetch_policy = if refresh_cache {
+        FetchPolicy::Always
+    } else {
+        FetchPolicy::IfNotExists
+    };
+
     let importer = TmdbImporter::new(state.tmdb.clone(), state.db.clone());
 
-    let data = importer.import(tmdb_id, FetchPolicy::IfNotExists).await?;
+    let data = importer.import(tmdb_id, fetch_policy).await?;
 
     Ok(Json(data))
 }
--- agin-tv/server/src/util/mod.rs
+++ agin-tv/server/src/util/mod.rs
@@ -1,3 +1,5 @@
 pub mod bool_from_string;
 pub mod tmdb;
 pub mod tokens;
+
+pub use bool_from_string::*;
