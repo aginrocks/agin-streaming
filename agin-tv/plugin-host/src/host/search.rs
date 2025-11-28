3c4096e08f82e222a05a4b02a0985e5d4f8a1303
Tymek <vonautymek@gmail.com>
2025-11-28 21:35:23 +0100

    tv/plugin-host: amde search concurrent

--- agin-tv/plugin-host/src/host/search.rs
+++ agin-tv/plugin-host/src/host/search.rs
@@ -1,35 +1,54 @@
-use plugin_proto::{SearchRequest, ServiceType};
+use futures::future::join_all;
+use plugin_proto::{SearchRequest, SearchResult, ServiceType};
+use tonic::Status;
 
-use crate::host::PluginHost;
+use crate::{host::PluginHost, plugin::Plugin};
 
 impl PluginHost {
-    pub async fn search(&self, request: SearchRequest) {
-        let mut results = Vec::new();
+    pub async fn search(&self, request: SearchRequest) -> Vec<SearchResult> {
+        let jobs = self
+            .plugins
+            .iter()
+            .map(|entry| {
+                let plugin = entry.value();
+                self.dispatch_search(plugin.clone(), request.clone())
+            })
+            .collect::<Vec<_>>();
 
-        for entry in self.plugins.iter() {
-            let plugin = entry.value();
-            if plugin
-                .info
-                .services
-                .iter()
-                .any(|s| s.r#type == ServiceType::Search as i32)
-            {
-                let mut services = plugin.clients.write().await;
-                match services.search.search(request.clone()).await {
-                    Ok(services) => {
-                        let response = services.into_inner();
-                        results.extend(response.results);
-                    }
-                    Err(e) => {
-                        tracing::warn!(
-                            "Search request to plugin {}:{} failed: {}",
-                            plugin.hostname,
-                            plugin.port,
-                            e
-                        );
-                    }
-                }
+        join_all(jobs)
+            .await
+            .into_iter()
+            .filter_map(|r| r.ok())
+            .flatten()
+            .collect()
+    }
+
+    async fn dispatch_search(
+        &self,
+        plugin: Plugin,
+        request: SearchRequest,
+    ) -> Result<Vec<SearchResult>, Status> {
+        if plugin
+            .info
+            .services
+            .iter()
+            .any(|s| s.r#type == ServiceType::Search as i32)
+        {
+            let mut services = plugin.clients.write().await;
+            let result = services.search.search(request.clone()).await;
+
+            if let Err(e) = &result {
+                tracing::warn!(
+                    "Search request to plugin {}:{} failed: {}",
+                    plugin.hostname,
+                    plugin.port,
+                    e
+                );
             }
+
+            result.map(|res| res.into_inner().results)
+        } else {
+            Ok(Vec::new())
         }
     }
 }
