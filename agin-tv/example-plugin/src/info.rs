3124e4acf09cb08ba097d4c5398d1c0a0c9a8941
Tymek <vonautymek@gmail.com>
2025-12-05 00:43:52 +0100

    tv: fixed example plugin

--- agin-tv/example-plugin/src/info.rs
+++ agin-tv/example-plugin/src/info.rs
@@ -1,30 +1,14 @@
 use plugin_sdk::handler;
 use plugin_sdk::{
     context::Context,
-    handler::{HandlerError, HandlerInfo, HandlerMetadata, SearchHandler},
+    handler::HandlerError,
     plugin::{SearchRequest, SearchResponse},
 };
 
 use crate::State;
 
-pub fn info() -> HandlerMetadata<State> {
-    async fn inner(
-        ctx: Context<State>,
-        request: SearchRequest,
-    ) -> Result<SearchResponse, HandlerError> {
-        todo!()
-    }
-
-    HandlerMetadata {
-        providers: vec!["example-provider".to_string()],
-        info: HandlerInfo::Search(SearchHandler {
-            callback: |ctx, request| Box::pin(async move { inner(ctx, request).await }),
-        }),
-    }
-}
-
 #[handler(search, supports("example-provider"))]
-pub async fn test(
+pub async fn info(
     ctx: Context<State>,
     request: SearchRequest,
 ) -> Result<SearchResponse, HandlerError> {
--- agin-tv/example-plugin/src/main.rs
+++ agin-tv/example-plugin/src/main.rs
@@ -11,23 +11,11 @@ pub struct State {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let addr = "[::1]:50051".parse()?;
-    // let greeter = Info::default();
-    //
-    //
-
     let state = State { a: "a".into() };
 
     let sdk = PluginSdk::builder(state).add_services(vec![info()]);
 
     sdk.listen("0.0.0.0:50051".parse().unwrap()).await?;
-
-    // let sdk = PluginSdk::builder().add_service(InfoProviderServer::new(greeter));
-
-    // Server::builder()
-    //     .add_service(InfoProviderServer::new(greeter))
-    //     .serve(addr)
-    //     .await?;
 
     Ok(())
 }
