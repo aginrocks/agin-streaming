cbbf9c0a254f1945173b54080af9931200cbf195
Tymek <vonautymek@gmail.com>
2025-12-05 01:12:49 +0100

    tv/plugin-sdk: fixes

--- agin-tv/plugin-sdk-macros/src/handler/info.rs
+++ agin-tv/plugin-sdk-macros/src/handler/info.rs
@@ -4,13 +4,13 @@ use quote::quote;
 pub fn generate_info(inv: &Invocation) -> proc_macro2::TokenStream {
     let name = &inv.function.sig.ident;
     match inv.args.handler_type {
-        HandlerType::Search => search_callback(inv, name),
-        HandlerType::SourceProvider => source_provider_callback(inv, name),
-        HandlerType::LinkResolver => link_resolver_callback(inv, name),
+        HandlerType::Search => search_callback(name),
+        HandlerType::SourceProvider => source_provider_callback(name),
+        HandlerType::LinkResolver => link_resolver_callback(name),
     }
 }
 
-pub fn search_callback(inv: &Invocation, name: &proc_macro2::Ident) -> proc_macro2::TokenStream {
+pub fn search_callback(name: &proc_macro2::Ident) -> proc_macro2::TokenStream {
     quote! {
         ::plugin_sdk::handler::HandlerInfo::Search(::plugin_sdk::handler::SearchHandler {
             callback: |ctx, request| Box::pin(async move { #name(ctx, request).await }),
@@ -18,10 +18,7 @@ pub fn search_callback(inv: &Invocation, name: &proc_macro2::Ident) -> proc_macr
     }
 }
 
-pub fn source_provider_callback(
-    inv: &Invocation,
-    name: &proc_macro2::Ident,
-) -> proc_macro2::TokenStream {
+pub fn source_provider_callback(name: &proc_macro2::Ident) -> proc_macro2::TokenStream {
     quote! {
         ::plugin_sdk::handler::HandlerInfo::SourceProvider(::plugin_sdk::handler::SourceProviderHandler {
             callback: |ctx, request| Box::pin(async move { #name(ctx, request).await }),
@@ -29,12 +26,9 @@ pub fn source_provider_callback(
     }
 }
 
-pub fn link_resolver_callback(
-    inv: &Invocation,
-    name: &proc_macro2::Ident,
-) -> proc_macro2::TokenStream {
+pub fn link_resolver_callback(name: &proc_macro2::Ident) -> proc_macro2::TokenStream {
     quote! {
-        ::plugin_sdk::handler::oHandlerInfo::LinkResolver(::plugin_sdk::handler::LinkResolverHandler {
+        ::plugin_sdk::handler::HandlerInfo::LinkResolver(::plugin_sdk::handler::LinkResolverHandler {
             callback: |ctx, request| Box::pin(async move { #name(ctx, request).await }),
         })
     }
