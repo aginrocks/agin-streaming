f7407ba395fa915e844909fad2778eab5748092b
Tymek <vonautymek@gmail.com>
2025-12-11 13:40:36 +0100

    tv/plugin-sdk: removed unused dependencies

--- agin-tv/plugin-sdk/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rerun-if-changed=../proto/plugin.proto");
-    tonic_prost_build::compile_protos("../proto/plugin.proto")?;
-    Ok(())
-}
