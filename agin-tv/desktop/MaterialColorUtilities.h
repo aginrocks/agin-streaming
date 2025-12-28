9759868a124d0004665b63fb48633a76767ad4b6
Tymek <vonautymek@gmail.com>
2025-12-28 18:06:58 +0100

    tv/desktop: configured cmake for material-color-utilities

---
+++ agin-tv/desktop/MaterialColorUtilities.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// Core color space conversions
+#include "cpp/cam/cam.h"
+#include "cpp/cam/hct.h"
+
+// Color schemes
+#include "cpp/scheme/scheme_content.h"
+#include "cpp/scheme/scheme_expressive.h"
+#include "cpp/scheme/scheme_fidelity.h"
+#include "cpp/scheme/scheme_monochrome.h"
+#include "cpp/scheme/scheme_neutral.h"
+#include "cpp/scheme/scheme_tonal_spot.h"
+#include "cpp/scheme/scheme_vibrant.h"
+
+// Dynamic colors
+#include "cpp/dynamiccolor/dynamic_scheme.h"
+#include "cpp/dynamiccolor/material_dynamic_colors.h"
+
+// Utilities
+#include "cpp/blend/blend.h"
+#include "cpp/palettes/tones.h"
+#include "cpp/utils/utils.h"
